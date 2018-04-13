82e287d94f5070ac20a73970557cf911d283dd21
hjkim666 <hjkim666@nate.com>
2018-04-13 11:26:50 +0900

    source

---
+++ 5day/mnist_pca.py
@@ -0,0 +1,28 @@
+from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
+digits = load_digits()
+
+fig, axes = plt.subplots(2, 5, figsize=(10, 5),
+                         subplot_kw={'xticks':(), 'yticks': ()})
+for ax, img in zip(axes.ravel(), digits.images):
+    ax.imshow(img)
+plt.show()    
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+pca.fit(digits.data)
+# 처음 두 개의 주성분으로 숫자 데이터를 변환
+digits_pca = pca.transform(digits.data)
+colors = ["#476A2A", "#7851B8", "#BD3430", "#4A2D4E", "#875525",
+          "#A83683", "#4E655E", "#853541", "#3A3120","#535D8E"]
+plt.figure(figsize=(10, 10))
+plt.xlim(digits_pca[:, 0].min(), digits_pca[:, 0].max())
+plt.ylim(digits_pca[:, 1].min(), digits_pca[:, 1].max())
+for i in range(len(digits.data)):
+    # 숫자 텍스트를 이용해 산점도
+    plt.text(digits_pca[i, 0], digits_pca[i, 1], str(digits.target[i]),
+             color = colors[digits.target[i]],
+             fontdict={'weight': 'bold', 'size': 9})
+plt.xlabel("first principal component")
+plt.ylabel("first second component")
+plt.show()
